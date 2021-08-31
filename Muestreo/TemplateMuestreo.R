17f4141a215f71ca6463a3baa2908917d4abe3ea
Juanjo <jgaleanoa@unal.edu.co>
2021-08-31 12:59:25 -0500

    Adding TemplateMuestreo file

---
+++ Muestreo/TemplateMuestreo.R
@@ -0,0 +1,7 @@
+obj <- c("mu_tau_con", "con_equals_sample_size", "prop_tot_con",
+         "tau_muc_mu", "tau_muc_mu_r", "sample_size", "Estimacionesppt",
+         "samplesizeppt", "EstimacionesPA",
+         "first", "tau_mu_2", "EstRazMu", "EstRazProp", "Estimacionesppt2",
+         "EstPoliReem", "biphase_equal_size_mu", "sample_size_biphase_equal")
+
+rm(list = ls()[!(ls() %in% obj)])
